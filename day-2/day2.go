132b7b1c7e2df4296b7d6bb3fee89c3eaddc2171
Ian Miller <imiller445@gmail.com>
2020-12-05 17:49:09 -0700

    Day 2 part 2 complete

--- day-2/day2.go
+++ day-2/day2.go
@@ -14,23 +14,38 @@ func main() {
 		return
 	}
 	passwordInputArray := strings.Split(string(passwordBytes), "\n")
-	validCount := 0
+	part1ValidCount := 0
+	part2ValidCount := 0
 	for _, passwordRule := range passwordInputArray {
-		lowCut := strings.Split(passwordRule, "-")
-		low, _ := strconv.Atoi(lowCut[0])
-		highCut := strings.Split(lowCut[1], " ")
-		high, _ := strconv.Atoi(highCut[0])
-		letter := strings.Split(highCut[1], ":")[0]
-		password := highCut[2]
-		if isValid(low, high, letter, password) {
-			validCount++
+		firstCut := strings.Split(passwordRule, "-")
+		firstValue, _ := strconv.Atoi(firstCut[0])
+		secondCut := strings.Split(firstCut[1], " ")
+		secondValue, _ := strconv.Atoi(secondCut[0])
+		letter := strings.Split(secondCut[1], ":")[0]
+		password := secondCut[2]
+		//fmt.Println(firstValue, secondValue, letter,password)
+		if isValidPart1(firstValue, secondValue, letter, password) {
+			part1ValidCount++
+		}
+		if isValidPart2(firstValue, secondValue, letter, password) {
+			part2ValidCount++
 		}
 	}
 
-	fmt.Println("The number of valid passwords is: ", validCount)
+	fmt.Println("The number of valid passwords for part 1 is: ", part1ValidCount)
+	fmt.Println("The number of valid passwords for part 2 is: ", part2ValidCount)
 }
 
-func isValid(low int, high int, letter string, password string) (valid bool) {
+func isValidPart1(low int, high int, letter string, password string) (valid bool) {
 	occurrences := strings.Count(password, letter)
 	return occurrences >= low && occurrences <= high
+}
+
+func isValidPart2(firstIndex int, secondIndex int, letter string, password string) (valid bool) {
+	i := password[firstIndex-1]
+	j := password[secondIndex-1]
+	isValid := ((i == letter[0] && j != letter[0]) ||
+		(i != letter[0] && j == letter[0])) &&
+		(i == letter[0] || j == letter[0])
+	return isValid
 }
